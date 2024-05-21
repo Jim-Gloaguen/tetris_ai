648c08eb2e21387722987b68ae97aacd933e2e25
gloaguenjim@gmail.com <gloaguenjim@gmail.com>
2024-05-21 08:30:11 +0200

    training

--- common.py
+++ common.py
@@ -15,11 +15,11 @@ GAME_TYPE = 'regular'
 
 #   2.  folder name to store dataset and model. './anything_you_like/'
 # FOLDER_NAME = './tetris_extra/'
-FOLDER_NAME = './tetris_regular_cnn_v1_oct_27/'
+FOLDER_NAME = './tetris_dataset_and_model/'
 
 #   3.  if > 0, then model {FOLDER_NAME}/whole_model/outer_{OUT_START} will be loaded to continue training or watch it play
 #       if 0, then create a brand new model.
-OUT_START = 9
+OUT_START = 0
 
 #   4.  choose the mode
 # MODE = 'human_player'
